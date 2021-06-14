b131e7b70168fd90c02e7afcaa3ce00c3738c8b2
Giang Le <gianglevan94@gmail.com>
2021-06-14 22:36:19 +0700

    Add annotation text

---
+++ src/components/Annotation.js
@@ -0,0 +1,42 @@
+import React, { useRef, useEffect, useState } from 'react';
+
+const Annotation = ({xScale, yScale, chartRef, data, margin}) => {
+  const divRef = useRef(null)
+  const [divStyle, setDivStyle] = useState({})
+  useEffect(() => {
+    const {top: chartTop, left: chartLeft, right: chartRight} = chartRef.current.getBoundingClientRect()
+    const {width, height} = divRef.current.getBoundingClientRect()
+    const x = xScale(data.index)
+    const y = yScale(data.clicks)
+    let top = chartTop + y + margin.top - height - 10
+    let left = chartLeft + x + margin.left - width / 2
+
+    if (left + width + margin.left + margin.right > chartRight) {
+      left = chartRight - width - margin.left - margin.right - 5
+    }
+
+    if (left < chartLeft + margin.left) {
+      left = chartLeft + margin.left
+      console.log(2312312)
+    }
+
+
+    setDivStyle({
+      top,
+      left,
+      visibility: 'visible'
+    })
+  }, [])
+
+  const getAnnotationStyle = () => {
+
+  }
+
+  return (
+    <div ref={divRef} style={divStyle} className="annotation-item">
+      {data.annotation}
+    </div>
+  );
+};
+
+export default Annotation;
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,12 +1,14 @@
-import React, {useEffect, useState, useRef, useMemo} from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
 import * as d3 from 'd3'
 
 import './line-chart.css'
+import Annotation from './Annotation';
 
 const _data = [
   {
     date: 'Test',
     clicks: 10,
+    annotation: 'First annotation'
   },
   {
     date: 'Test 1',
@@ -15,6 +17,7 @@ const _data = [
   {
     date: 'Test 2',
     clicks: 5,
+    annotation: 'This is test',
   },
   {
     date: 'Test 3',
@@ -31,10 +34,42 @@ const _data = [
   {
     date: 'Test 6',
     clicks: 40,
+    annotation: 'This is test annotation',
   },
 ]
 
-const TICKS = 6
+const data2 = [
+  {
+    date: 'Test',
+    clicks: 20,
+  },
+  {
+    date: 'Test 1',
+    clicks: 10,
+  },
+  {
+    date: 'Test 2',
+    clicks: 25,
+  },
+  {
+    date: 'Test 3',
+    clicks: 30,
+  },
+  {
+    date: 'Test 4',
+    clicks: 80,
+  },
+  {
+    date: 'Test 5',
+    clicks: 10,
+  },
+  {
+    date: 'Test 6',
+    clicks: 30,
+  },
+]
+
+const TICKS = 4
 
 const LineChart = () => {
   const [data, setData] = useState(_data)
@@ -45,7 +80,7 @@ const LineChart = () => {
   useEffect(() => {
     renderChart()
   }, [])
-
+  const max = Math.max(...data.map(d => d.clicks), ...data2.map(d => d.clicks))
   const margin = {top: 50, right: 50, bottom: 50, left: 50};
   const width = 800 - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
@@ -55,7 +90,7 @@ const LineChart = () => {
     .range([0, width])
 
   const yScale = d3.scaleLinear()
-    .domain([0, 60])
+    .domain([0, max])
     .range([height, 0])
 
   const tooltipStyle = useMemo(() => {
@@ -64,13 +99,11 @@ const LineChart = () => {
     }
     const {left: chartLeft, top: chartTop} = chartRef.current.getBoundingClientRect()
     const {width: tooltipWidth, height: tooltipHeight} = tooltipRef.current.getBoundingClientRect()
-    console.log(tooltipWidth)
     let left = chartLeft + xScale(tooltip.index) + margin.left - tooltipWidth / 2
     let top = chartTop + yScale(tooltip.clicks) + margin.top - tooltipHeight - 10
-    if(top < chartTop) {
+    if (top < chartTop) {
       top = chartTop + yScale(tooltip.clicks) + tooltipHeight + 10
     }
-
 
     return {
       left,
@@ -79,8 +112,21 @@ const LineChart = () => {
     }
   }, [tooltip, chartRef.current, tooltipRef.current])
 
-  const renderChart = () => {
+  const annotations = useMemo(() => {
+    const result = []
+    data.forEach((item, index) => {
+      if (item.annotation) {
+        result.push({
+          ...item,
+          index,
+        })
+      }
+    })
 
+    return result
+  }, [data])
+
+  const renderChart = () => {
     const lineGenerator = d3.line()
       .x((d, i) => xScale(i))
       .y(d => yScale(d.clicks))
@@ -96,26 +142,50 @@ const LineChart = () => {
     svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(xScale).ticks(TICKS));
+      .call(d3.axisBottom(xScale).tickFormat((_, i) => data[i].date));
 
     svg.append("g")
       .attr("class", "y axis")
       .call(d3.axisLeft(yScale).ticks(TICKS));
+
+    //Grid line
+    svg.append("g")
+      .attr("class", "grid")
+      .call(d3.axisLeft(yScale).ticks(TICKS)
+        .tickSize(-width)
+        .tickFormat("").tickSizeOuter(0)
+      )
+
+    svg.append("g")
+      .attr("class", "grid")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale)
+        .tickSize(-height)
+        .tickFormat("").tickSizeOuter(0)
+      )
 
 
     svg.append("path")
       .datum(data)
       .attr("class", "line")
       .attr('fill', 'none')
-      .attr('stroke', '#000')
-      .attr('stroke-width', 3)
+      .attr('stroke', '#8884d8')
+      .attr('stroke-width', 2)
       .attr("d", lineGenerator);
 
+    svg.append("path")
+      .datum(data2)
+      .attr("class", "line")
+      .attr('fill', 'none')
+      .attr('stroke', '#82ca9d')
+      .attr('stroke-width', 2)
+      .attr("d", lineGenerator);
 
-    svg.selectAll(".dot")
+    svg.append('g').selectAll("circle")
       .data(data)
       .enter().append("circle")
       .attr("class", "dot")
+      .attr("fill", "#8884d8")
       .attr("cx", (d, i) => xScale(i))
       .attr("cy", d => yScale(d.clicks))
       .attr("r", 5)
@@ -129,14 +199,36 @@ const LineChart = () => {
       })
       .on("mouseout", (d, index, dots) => {
         const dot = dots[index]
-        dot.style.fill = '#ffab00'
+        dot.style.fill = '#8884d8'
+        setTooltip(null)
+      })
+
+    svg.append('g').selectAll("circle-2")
+      .data(data2)
+      .enter().append("circle")
+      .attr("class", "dot2")
+      .attr("fill", "#82ca9d")
+      .attr("cx", (d, i) => xScale(i))
+      .attr("cy", d => yScale(d.clicks))
+      .attr("r", 5)
+      .on("mouseover", (d, index, dots) => {
+        const dot = dots[index]
+        dot.style.fill = 'red'
+        setTooltip({
+          ...d,
+          index,
+        })
+      })
+      .on("mouseout", (d, index, dots) => {
+        const dot = dots[index]
+        dot.style.fill = '#82ca9d'
         setTooltip(null)
       })
   }
 
   return (
     <div>
-      <div ref={chartRef} />
+      <div ref={chartRef}/>
 
       <div ref={tooltipRef} style={tooltipStyle} className="chart-tooltip">
         <div className="date">15/10/2020</div>
@@ -146,9 +238,13 @@ const LineChart = () => {
         </div>
         <div className="property">
           <span className="label">Clicks:</span>
-          <span className="value">{tooltip?.click}</span>
+          <span className="value">{tooltip?.clicks}</span>
         </div>
       </div>
+
+      {annotations.map((item, index) => (
+        <Annotation key={index} data={item} xScale={xScale} yScale={yScale} chartRef={chartRef} margin={margin}/>
+      ))}
     </div>
   );
 };
